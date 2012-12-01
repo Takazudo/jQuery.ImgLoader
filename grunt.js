43d923bc9bcfd9bd62ee1992caa57ee8eb64779c
Takeshi Takatsudo <takazudo@gmail.com>
2012-12-02 02:14:51 +0900

    oops minified file was old

--- grunt.js
+++ grunt.js
@@ -39,6 +39,6 @@ module.exports = function(grunt){
     }
   });
 
-  grunt.registerTask('default', 'coffee concat notifyOK');
+  grunt.registerTask('default', 'coffee concat uglify notifyOK');
 
 };
